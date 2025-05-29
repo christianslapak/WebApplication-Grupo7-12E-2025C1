d6c57a0f4e5797920a656570e9d9e48397a96871
christian.slapak <christian.slapak@ort.edu.ar>
2025-05-28 22:24:19 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication-Grupo7-12E-2025C1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_Grupo7_12E_2025C1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
